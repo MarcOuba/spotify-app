2c2faa08d89e71508d7f70a4708b5ba6f2c1db75
MarcOuba <marcsss123@gmail.com>
2023-10-05 14:59:48 +0300

    setup gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://marcouba.github.io/spotify-app",
+
   plugins: [react()],
 })
